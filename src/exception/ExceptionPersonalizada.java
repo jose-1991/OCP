511d8e30eb8aa152cbfc172ef5bc336a68307e59
jose-1991 <jose.floresc.java@gmail.com>
2021-07-02 13:09:10 -0400

    excepciones personalizadas

--- src/exception/ExceptionPersonalizada.java
+++ src/exception/ExceptionPersonalizada.java
@@ -2,6 +2,7 @@ package exception;
 
 import exception.personalizada.Calculadora;
 import exception.personalizada.DivisionPorCeroException;
+import exception.personalizada.FormatoNumeroException;
 import generics.modelos.Camion;
 
 import javax.swing.*;
@@ -9,20 +10,18 @@ import javax.swing.*;
 public class ExceptionPersonalizada {
     public static void main(String[] args) {
         Calculadora cal = new Calculadora();
-        String valor = JOptionPane.showInputDialog("ingrese un numero entero");
-        int divisor;
-        double division;
+        String numerador = JOptionPane.showInputDialog("ingrese entero numerador");
+        String denominador = JOptionPane.showInputDialog("ingrese un entero denominador");
 
         try{
-            divisor = Integer.parseInt(valor);
-            division = cal.dividir(10,divisor);
-            System.out.println(division);
-        }catch (NumberFormatException e){
+           double division = cal.dividir(numerador, denominador);
+            System.out.println("division = " + division);
+        }catch (FormatoNumeroException e){
             System.out.println("ingrese un valor numerico: "+e.getMessage());
-            main(args);
+
         }catch (DivisionPorCeroException de){
             System.out.println("se capturo una exception: "+de.getMessage());
-            main(args);
+
         }
     }
 
--- src/exception/personalizada/Calculadora.java
+++ src/exception/personalizada/Calculadora.java
@@ -9,4 +9,15 @@ public class Calculadora {
         }
         return numerador/(double)divisor;
     }
+
+    public double dividir(String numerador, String divisor)
+            throws DivisionPorCeroException, FormatoNumeroException{
+        try{
+            int num = Integer.parseInt(numerador);
+            int div = Integer.parseInt(divisor);
+            return  this.dividir(num, div);
+        }catch (NumberFormatException e){
+            throw new FormatoNumeroException("debe ingresar un numero en el numerador y en el divisor");
+        }
+    }
 }
