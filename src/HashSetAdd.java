38024e73d45c3abf8d492243a9b468bc3c110100
jose-1991 <jose.floresc.java@gmail.com>
2021-07-05 10:30:02 -0400

    metodo add HashSet

---
+++ src/HashSetAdd.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetAdd {
+    public static void main(String[] args) {
+        Set <String> hs = new HashSet<>();
+        System.out.println(hs.add("uno"));
+        System.out.println(hs.add("dos"));
+        System.out.println(hs.add("tres"));
+        System.out.println(hs.add("cuatro"));
+        System.out.println(hs.add("cinco"));
+
+        boolean duplicado = hs.add("tres");
+
+        System.out.println("permite elementos duplicados?: "+ duplicado);
+        System.out.println(hs);
+    }
+}
