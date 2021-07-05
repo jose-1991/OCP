ab8846a0405deb3f11657aed189c2609354ee353
jose-1991 <jose.floresc.java@gmail.com>
2021-07-05 13:47:56 -0400

    hashSet para evitar duplicado con objetos, y formas de iteracion de colecciones

---
+++ src/estructuraDeDatos/HashSetEqualsHashCode.java
@@ -0,0 +1,51 @@
+package estructuraDeDatos;
+
+import estructuraDeDatos.modelo.Alumno;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class HashSetEqualsHashCode {
+    public static void main(String[] args) {
+        Set<Alumno> sa = new HashSet<>();
+        sa.add(new Alumno("Pato",5));
+        sa.add(new Alumno("Cata",6));
+        sa.add(new Alumno("Luci",4));
+        sa.add(new Alumno("Jano",7));
+        sa.add(new Alumno("Andres",3));
+        sa.add(new Alumno("Zeus",2));
+        sa.add(new Alumno("Zeus",2));
+        sa.add(new Alumno("Lucas",4));
+
+        //se sobreescribio el metodo equals y el hashcode en la Clase Alumno
+        //para que el HashSet no acepte duplicados
+
+        System.out.println(sa);
+
+        //formas de iterar las colecciones
+
+        // el for estandar solo se usa con List, con el metodo Get()
+
+        /*for (int i = 0; i<sa.size(); i++){
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }*/
+
+
+        System.out.println("=========Iterando con ForEach============");
+
+        for (Alumno a: sa){
+            System.out.println(a.getNombre());
+        }
+        System.out.println("=========Iterando con While y el Iterator============");
+        Iterator<Alumno> it = sa.iterator();
+        while (it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+        System.out.println("=========Iterando con Stream ForEach============");
+        sa.forEach(System.out::println);
+    }
+}
--- src/estructuraDeDatos/modelo/Alumno.java
+++ src/estructuraDeDatos/modelo/Alumno.java
@@ -1,5 +1,7 @@
 package estructuraDeDatos.modelo;
 
+import java.util.Objects;
+
 public class Alumno implements Comparable<Alumno> {
     private String nombre;
     private Integer nota;
@@ -44,5 +46,18 @@ public class Alumno implements Comparable<Alumno> {
         if (this.nota == null){
             return 0;
         } return this.nota.compareTo(a.nota);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre) && Objects.equals(nota, alumno.nota);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, nota);
     }
 }
