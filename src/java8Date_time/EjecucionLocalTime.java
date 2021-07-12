6152c781781ee56cc9604c267dfd3e6adac6258c
jose-1991 <jose.floresc.java@gmail.com>
2021-07-12 11:26:33 -0400

    LocalDateTime, DateTimeFormatter

--- src/java8Date_time/EjecucionLocalTime.java
+++ src/java8Date_time/EjecucionLocalTime.java
@@ -3,6 +3,7 @@ package java8Date_time;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class EjecucionLocalTime {
@@ -15,16 +16,30 @@ public class EjecucionLocalTime {
         System.out.println("Minutos: "+ ahora.getMinute());
         System.out.println("Segundos: "+ ahora.getSecond());
 
-        LocalTime seistreinta = LocalTime.of(6,30);
+        LocalTime seistreinta = LocalTime.of(6,30,59);
         System.out.println(seistreinta);
 
-        seistreinta = LocalTime.parse("6:30");
+        seistreinta = LocalTime.parse("06:30");
         System.out.println("seistreinta = " + seistreinta);
 
         LocalTime sieteTreinta = LocalTime.of(6,30).plus(1, ChronoUnit.HOURS);
         System.out.println("sieteTreinta = " + sieteTreinta);
 
-        boolean esAnterior = LocalTime.of(6,30).isBefore(LocalTime.parse("07:30"));
+        boolean esAnterior = LocalTime.of(6,30,59).isBefore(LocalTime.parse("07:30"));
+        System.out.println("esAnterior = " + esAnterior);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        String ahoraFormateado = seistreinta.format(df);
+        System.out.println(ahoraFormateado);
+
+        ahoraFormateado = df.format(ahora);
+        System.out.println("ahoraFormateado = " + ahoraFormateado);
+
+        LocalTime max = LocalTime.MAX;
+        LocalTime min = LocalTime.MIN;
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
 
 
 
