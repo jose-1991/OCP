6b83772ebc522848594dbf42cb18b37649cffaa9
jose-1991 <jose.floresc.java@gmail.com>
2021-07-15 09:59:23 -0400

    Patron Singleton

---
+++ src/patronesDiseno/EjecucionSingleton.java
@@ -0,0 +1,19 @@
+package patronesDiseno;
+
+import patronesDiseno.singleton.ConexionBDSingleton;
+
+public class EjecucionSingleton {
+    public static void main(String[] args) {
+
+        ConexionBDSingleton con = null;
+        for (int i = 0; i<5 ; i++){
+            con = ConexionBDSingleton.getInstancia();
+            System.out.println("con = " + con);
+        }
+
+        ConexionBDSingleton con2 = ConexionBDSingleton.getInstancia();
+
+        boolean b1 = (con==con2);
+        System.out.println("b1 = " + b1);
+    }
+}
---
+++ src/patronesDiseno/singleton/ConexionBDSingleton.java
@@ -0,0 +1,16 @@
+package patronesDiseno.singleton;
+
+public class ConexionBDSingleton {
+    private static ConexionBDSingleton instancia;
+
+    private ConexionBDSingleton(){
+        System.out.println("Conentandose a algun motor de base de datos");
+    }
+
+    public static ConexionBDSingleton getInstancia(){
+        if (instancia == null){
+            instancia = new ConexionBDSingleton();
+        }
+        return instancia;
+    }
+}
