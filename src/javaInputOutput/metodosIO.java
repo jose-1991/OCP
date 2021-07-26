1ce6630066d0ac8c2bbd3d233c1fcf8b3da92137
jose-1991 <jose.floresc.java@gmail.com>
2021-07-26 13:44:27 -0400

    I/O metodos

---
+++ src/javaInputOutput/metodosIO.java
@@ -0,0 +1,41 @@
+package javaInputOutput;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class metodosIO {
+    public static void main(String[] args) throws IOException {
+        System.out.println("============  ReadAllLines  ============");
+        List<String> stream = Files.readAllLines(Paths.get("test.txt"));
+        System.out.println(stream);
+//        for (String h: stream){
+//            System.out.println(h);
+//        }
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("test.txt");
+            out = new FileOutputStream("dest.txt");
+            int c;
+
+            while ((c = in.read()) != -1){
+                out.write(c);
+            }
+        }finally {
+            if (in != null){
+                in.close();
+            }
+            if (out != null){
+                out.close();
+            }
+        }
+
+
+    }
+}
